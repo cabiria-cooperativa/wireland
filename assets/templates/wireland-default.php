ba218b2e0ff1651006b9b2b1427e5d26653cba31
Simone Alati <git@simonealati.it>
2019-03-11 10:02:23 +0100

    no message

--- assets/templates/wireland-default.php
+++ assets/templates/wireland-default.php
@@ -15,7 +15,7 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700">
-        <link rel="stylesheet" href="<?php echo plugin_dir_url( __FILE__ ) ?>../css/style.css">
+        <link rel="stylesheet" href="<?php echo plugin_dir_url( __FILE__ ) ?>../css/style.css?ver=<?php echo mt_rand() ?>">
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
